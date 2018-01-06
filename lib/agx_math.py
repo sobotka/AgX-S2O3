845b39bf6cb28ba10b519a2bf3e4cf7394378f00
Troy James Sobotka <troy.sobotka@gmail.com>
2018-01-06 09:57:32 -0800

    Streamline math routines
    Removed redundant float versions of the workhorse
    functions and reworked them to return arrays or
    single floats as needed. Hat tip to the ever wise
    and patient Thomas Mansencal.

--- lib/agx_math.py
+++ lib/agx_math.py
@@ -8,26 +8,35 @@ Defines utility functions.
 import numpy
 
 
-def calculateYfromXQuadratic(cP, inX):
-    for inValue in inX:
-        if (len(cP)) <= 0:
+def as_numeric(obj, as_type=numpy.float64):
+    try:
+        return as_type(obj)
+    except TypeError:
+        return obj
+
+
+def calculateYfromXQuadratic(cP, inValues):
+    inValues = numpy.asarray(inValues)
+    outY = numpy.zeros(inValues.size)
+    cP = numpy.asarray(cP)
+
+    for index, inValue in numpy.ndenumerate(inValues):
+        if len(cP) <= 0:
             raise ValueError(
-                "Invalid number of control points for a quadratic curve."
+                "Invalid number of control points for a quadratic curve.\n"
+                "\tQuadratic curves must have at least one set of three "
+                "control points."
             )
 
         for curCP in cP:
             if len(curCP) != 3:
                 raise ValueError(
                     "Invalid number of control points for a quadratic curve.\n"
-                    "Quadratic curves require three control points per "
+                    "\tQuadratic curves require three control points per "
                     "section."
                 )
 
             if curCP[0][0] <= inValue <= curCP[2][0]:
-                if inValue == 1.:
-                    yield 1.
-                    break
-
                 coefficients = [
                     curCP[0][0] - (2. * curCP[1][0]) + curCP[2][0],
                     (2. * curCP[1][0]) - (2. * curCP[0][0]),
@@ -38,69 +47,80 @@ def calculateYfromXQuadratic(cP, inX):
                 correct_root = None
 
                 for root in roots:
-                    if numpy.isreal(root) and 0. <= root <= 1.:
+                    if numpy.isreal(root) and (0. <= root < 1.):
                         correct_root = root
+                    elif inValue == 1.:
+                        correct_root = 1.
 
                 if correct_root is not None:
                     root_t = correct_root
-                    outY = (((1. - root_t)**2. * curCP[0][1]) +
+                    outV = (((1. - root_t)**2. * curCP[0][1]) +
                             (2. * (1. - root_t) * root_t * curCP[1][1]) +
                             (root_t**2. * curCP[2][1]))
-                    yield outY
+                    outY[index] = outV
                     break
 
                 if correct_root is None:
+                    print("*** ERROR ***: inValue[", index, "]: ", inValue)
                     raise ValueError(
-                        "No valid root found for coefficients. Invalid curve or "
-                        "input x value."
+                        "No valid root found for coefficients. Invalid curve "
+                        "or input x value."
                     )
+    return as_numeric(outY)
 
 
-def calculateYfromXCubic(cP, inX):
-    for inValue in inX:
-        if (len(cP)) <= 0:
+def calculateYfromXCubic(cP, inValues):
+    inValues = numpy.asarray(inValues)
+    outY = numpy.zeros(inValues.size)
+    cP = numpy.asarray(cP)
+
+    for index, inValue in numpy.ndenumerate(inValues):
+        if len(cP) <= 0:
             raise ValueError(
                 "Invalid number of control points for a cubic curve."
             )
 
-        for index, curCP in enumerate(cP):
+        for curCP in cP:
             if len(curCP) != 4:
                 raise ValueError(
                     "Invalid number of control points for a cubic curve.\n"
-                    "Cubic curves require four control points per "
+                    "\tCubic curves require four control points per "
                     "section."
                 )
 
-            coefficients = [
-                -curCP[0][0] + 3. * curCP[1][0] - 3. * curCP[2][0] +
-                curCP[3][0],
-                3. * curCP[0][0] - 6 * curCP[1][0] + 3 * curCP[2][0],
-                -3 * curCP[0][0] + 3 * curCP[1][0],
-                curCP[0][0] - inValue
-            ]
+            if curCP[0][0] <= inValue <= curCP[3][0]:
+                coefficients = [
+                    -curCP[0][0] + 3. * curCP[1][0] - 3. * curCP[2][0] +
+                    curCP[3][0],
+                    3. * curCP[0][0] - 6 * curCP[1][0] + 3 * curCP[2][0],
+                    -3 * curCP[0][0] + 3 * curCP[1][0],
+                    curCP[0][0] - inValue
+                ]
 
-            roots = numpy.roots(coefficients)
-            correct_root = None
+                roots = numpy.roots(coefficients)
+                correct_root = None
 
-            for root in roots:
-                if numpy.isreal(root) and 0. <= root <= 1.:
-                    correct_root = root
+                for root in roots:
+                    if numpy.isreal(root) and (0. <= root <= 1.):
+                        correct_root = root
 
-            if correct_root is not None:
-                root_t = correct_root
-                outY = ((1. - root_t)**3. * curCP[0][1] + 3. *
+                if correct_root is not None:
+                    root_t = correct_root
+                    outY[index] = (
+                        (1. - root_t)**3. * curCP[0][1] + 3. *
                         (1. - root_t)**2. * root_t * curCP[1][1] + 3. *
                         (1. - root_t) * root_t**2. * curCP[2][1] + root_t**3.
                         * curCP[3][1])
-                break
+                    break
 
-        if correct_root is None:
-            raise ValueError(
-                "No valid root found for coefficients. Invalid curve or "
-                "input x value."
-            )
+                if correct_root is None:
+                    raise ValueError(
+                        "No valid root found for coefficients. Invalid curve "
+                        "or input x value. Control Point: ", curCP,
+                        " Index Value[", index, "]: ", inValue[index]
+                    )
 
-        yield outY
+    return as_numeric(outY)
 
 
 def calculateIntercept(dispIn, logIn, slope):
@@ -130,51 +150,34 @@ def calculateLinToLog(inLin, middleGrey, minExposure, maxExposure):
     # 2^stops * middleGrey = linear
     # log(2)*stops * middleGrey = log(linear)
     # stops = log(linear)/log(2)*middleGrey
-    for inValue in inLin:
+    inLin = numpy.asarray(inLin)
+    outLog = numpy.zeros(inLin.size)
+
+    for index, inValue in numpy.ndenumerate(inLin):
         if (inValue <= 0.):
-            yield 0.
+            outLog[index] = 0.
 
         lg2 = numpy.log2(inValue / middleGrey)
-        logNorm = (lg2 - minExposure) / (maxExposure - minExposure)
+        outLog[index] = (lg2 - minExposure) / (maxExposure - minExposure)
 
-        if(logNorm < 0.):
-            yield 0.
+        if(outLog[index] < 0.):
+            outLog[index] = 0.
 
-        yield logNorm
-
-
-def calculateLinToLog_f(inLin, middleGrey, minExposure, maxExposure):
-    if (inLin <= 0.):
-        return 0.
-
-    lg2 = numpy.log2(inLin / middleGrey)
-    logNorm = (lg2 - minExposure) / (maxExposure - minExposure)
-
-    if(logNorm < 0.):
-        return 0.
-
-    return logNorm
+    return as_numeric(outLog)
 
 
 def calculateLogToLin(inLogNorm, middleGrey, minExposure, maxExposure):
-    for inValue in inLogNorm:
+    inLogNorm = numpy.asarray(inLogNorm)
+    outLin = numpy.zeros(inLogNorm.size)
+
+    for index, inValue in numpy.ndenumerate(inLogNorm):
         if (inValue < 0.):
-            yield 0.
+            outLin[index] = 0.
 
         lg2 = inValue * (maxExposure - minExposure) + minExposure
-        lin = pow(2., lg2) * middleGrey
+        outLin[index] = pow(2., lg2) * middleGrey
 
-        yield lin
-
-
-def calculateLogToLin_f(inLogNorm, middleGrey, minExposure, maxExposure):
-    if (inLogNorm < 0.):
-        return 0.
-
-    lg2 = inLogNorm * (maxExposure - minExposure) + minExposure
-    lin = pow(2., lg2) * middleGrey
-
-    return lin
+    return as_numeric(outLin)
 
 
 def calculateStopsToLin(inStops, middleGrey):
